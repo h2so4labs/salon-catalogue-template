083f30fd1ecc3c8100725ec7954434608dd99c88
PrateekSavanur <prateek.savanur@gmail.com>
2024-07-18 12:25:23 +0530

    Footer and Gallery changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function showGallery(galleryId) {
-  const galleries = document.querySelectorAll(".gallery-images");
-  galleries.forEach((gallery) => {
-    gallery.classList.remove("active");
+function showGallery(carouselId) {
+  console.log(carouselId);
+  const carousels = document.querySelectorAll(".gallery-images");
+  carousels.forEach((carousel) => {
+    carousel.classList.remove("active");
   });
-  document.getElementById(galleryId).classList.add("active");
+  document.getElementById(carouselId).classList.add("active");
 }
