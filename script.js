0b0db1eae92ce3ad6bb35b9ad70d39dd5d487a9c
PrateekSavanur <prateek.savanur@gmail.com>
2024-07-19 23:51:41 +0530

    Added Navbar and design updates

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function showGallery(carouselId) {
   });
   document.getElementById(carouselId).classList.add("active");
 }
+
+showGallery("salon");
+
+function closeNav() {
+  const navbarContent = document.getElementById("navbarContent");
+  navbarContent.classList.remove("show");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.getElementById("current-year");
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+});
