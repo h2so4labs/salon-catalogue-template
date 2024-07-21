a462b3a562d9a3aa9455f0f5d2ecad3c2d5b4098
Upadhyay <rajaupadhyay@deloitte.com>
2024-07-21 16:55:07 +0530

    changes

--- script.js
+++ script.js
@@ -22,11 +22,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const video = document.getElementById("hoverVideo");
 
-video.addEventListener("mouseenter", () => {
-  video.play();
+// document.addEventListener("DOMContentLoaded", function () {
+//   video.addEventListener("mouseenter", () => {
+//     video.play();
+//   });
+
+//   video.addEventListener("mouseleave", () => {
+//     video.pause();
+//     video.currentTime = 0;
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Check if phone exists in localStorage
+  console.log("checking for phone")
+  const phone = localStorage.getItem("phone");
+  console.log(phone)
+  if (phone) {
+    console.log("Found phone");
+    document.getElementById("accountLink-h2so4").style.display = "block";
+  }
 });
 
-video.addEventListener("mouseleave", () => {
-  video.pause();
-  video.currentTime = 0;
-});
+function toggleNavbar() {
+  const sidebar = document.getElementById("sidebar-h2so4");
+  const overlay = document.getElementById("overlay-h2so4");
+
+  if (sidebar.style.width === "250px") {
+    sidebar.style.width = "0";
+    overlay.classList.remove("show");
+  } else {
+    sidebar.style.width = "250px";
+    overlay.classList.add("show");
+  }
+}
